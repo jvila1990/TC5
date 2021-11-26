ae1c0b58ff17dc0a3914001c919118573da9f0b5
Jonathan <Jonathan@DESKTOP-1J9GJE4>
2021-11-26 00:43:16 -0500

    Agregar archivos de proyecto.

---
+++ TC5/Nodo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC5
+{
+    public class Nodo
+    {
+        public int valor;
+        public string valor2;
+        public string valor3;
+        public Nodo izquierdo;
+        public Nodo derecho;
+    }
+}
---
+++ TC5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinario arbol = new ArbolBinario();
+            arbol.InsertarNodo(5, "Claudia", "Vivas Paucar");
+            arbol.InsertarNodo(2, "Ernesto", "Salvatierra Rosas");
+            arbol.InsertarNodo(10, "Pamela", "Valdez Perez");
+            arbol.InsertarNodo(100, "Hilda", "Palacios Sanchez");
+            arbol.InsertarNodo(1, "Francisco", "Ortiz Garfias");
+            arbol.InsertarNodo(3, "Jose", "Rojas Arias");
+
+
+            //INORDEN - PREORDEN - POSTORDEN
+          
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Preorden:");
+            arbol.Preorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Inorden:");
+            arbol.Inorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Postorden:");
+            arbol.Postorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+
+            //ELIMINAR UN DATO
+
+            arbol.Eliminar(arbol.GetRaiz(), 100);
+            Console.WriteLine("");
+
+            arbol.Eliminar(arbol.GetRaiz(), 1);
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("SE HA ELIMINADO EL REGISTRO 100");
+            Console.WriteLine("SE HA ELIMINADO EL REGISTRO 1");
+            Console.WriteLine("------------------------");
+
+
+            //INORDEN - PREORDEN - POSTORDEN
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Preorden:");
+            arbol.Preorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Inorden:");
+            arbol.Inorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Postorden:");
+            arbol.Postorden(arbol.GetRaiz());
+            Console.WriteLine("");
+
+
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
